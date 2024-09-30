78e6cf9604edd5bf4bff2e10e564948ae4e7e499
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-09-30 07:11:54 +0600

    wrapping master

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view ('/', 'login');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
