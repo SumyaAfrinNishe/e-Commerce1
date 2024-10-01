a5fa4ecddef10190ac4d40cd1f9f04d8ef9fc2ee
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-10-02 03:39:15 +0600

    Seeding done

--- database/seeders/User_Seeder.php
+++ database/seeders/User_Seeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class User_Seeder extends Seeder
 {
@@ -12,6 +14,10 @@ class User_Seeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name'=> 'Afrin',
+            "email"=>"afrin@gmail.com",
+            "password"=> Hash::make('1234'),
+        ]);
     }
 }
