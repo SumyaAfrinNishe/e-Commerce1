04744662e9d484f60601849b0d9c7b009eb34056
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-10-02 04:52:04 +0600

    ProductController

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
-Route::view('/', 'login');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login',[UserController::class,'login']);
+
+Route::get('/',[ProductController::class,'index']);
